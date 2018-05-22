7f73ab24a2009e2f3a6a9e7536be20770c73c799
Jesada <58160341@go.buu.ac.th>
2018-05-23 04:48:59 +0700

    update data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,18 +12,18 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
     this.name = "Team 6";
     this.totalmem = 13;
-    this.members.push({id:1,nick:'Bam',fname:'Kontee',position:'Team Leader',img:'../img/bam.jpg'});
-    this.members.push({id:2,nick:'Waan',fname:'Porawee',position:'Planning Manager',img:'../img/waan.jpg'});
-    this.members.push({id:3,nick:'Ti',fname:'Aphisit',position:'Planning',img:'../img/ti.jpg'});
-    this.members.push({id:4,nick:'Muay',fname:'Arphaporn',position:'Planning',img:'../img/mauy.jpg'});
-    this.members.push({id:5,nick:'Nat',fname:'Jesada',position:'Deverlopment Manager',img:'../img/nat.jpg'});
-    this.members.push({id:6,nick:'Bew',fname:'Narongrit',position:'Deverlopment',img:'../img/bew.jpg'});
-    this.members.push({id:7,nick:'Fluke',fname:'Prachaya',position:'Deverlopment',img:'../img/fluke.jpg'});
-    this.members.push({id:8,nick:'Mos',fname:'Nattawut',position:'Quality assurerance Manager',img:'../img/mos.jpg'});
-    this.members.push({id:9,nick:'Nut',fname:'Nuttawan',position:'Quality assurerance',img:'../img/nut.jpg'});
-    this.members.push({id:10,nick:'Teen',fname:'Kasama',position:'Support Manager',img:'../img/teen.jpg'});
-    this.members.push({id:11,nick:'Pipe',fname:'Siwakorn',position:'Support',img:'../img/pipe.jpg'});
-    this.members.push({id:12,nick:'Gell',fname:'Khemjira',position:'Support',img:'../img/gell.jpg'});
+    this.members.push({id:1,nick:'Bam',fname:'Kontee',position:'Team Leader',img:'assets/img/bam_03.jpg'});
+    this.members.push({id:2,nick:'Waan',fname:'Porawee',position:'Planning Manager',img:'assets/img/waan_03.jpg'});
+    this.members.push({id:3,nick:'Ti',fname:'Aphisit',position:'Planning',img:'assets/img/ti_03.jpg'});
+    this.members.push({id:4,nick:'Muay',fname:'Arphaporn',position:'Planning',img:'assets/img/mauy_03.jpg'});
+    this.members.push({id:5,nick:'Nat',fname:'Jesada',position:'Deverlopment Manager',img:'assets/img/nat_03.jpg'});
+    this.members.push({id:6,nick:'Bew',fname:'Narongrit',position:'Deverlopment',img:'assets/img/bew_03.jpg'});
+    this.members.push({id:7,nick:'Fluke',fname:'Prachaya',position:'Deverlopment',img:'assets/img/fluke_03.jpg'});
+    this.members.push({id:8,nick:'Mos',fname:'Nattawut',position:'Quality assurerance Manager',img:'assets/img/mos_03.jpg'});
+    this.members.push({id:9,nick:'Nut',fname:'Nuttawan',position:'Quality assurerance',img:'assets/img/nut_03.jpg'});
+    this.members.push({id:10,nick:'Teen',fname:'Kasama',position:'Support Manager',img:'assets/img/teen_03.jpg'});
+    this.members.push({id:11,nick:'Pipe',fname:'Siwakorn',position:'Support',img:'assets/img/pipe_03.jpg'});
+    this.members.push({id:12,nick:'Gell',fname:'Khemjira',position:'Support',img:'assets/img/gell_03.jpg'});
     }
 
 }

e77081b53d09097e61b4c02651f78bef11e9e758
Jesada <58160341@go.buu.ac.th>
2018-05-23 03:07:13 +0700

    img

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,22 +8,22 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
   name: string;
   totalmem: number;
-  members: Array<{id: number, nick: string, fname: string, position: string}> = [];
+  members: Array<{id: number, nick: string, fname: string, position: string, img: string}> = [];
   constructor(public navCtrl: NavController) {
     this.name = "Team 6";
     this.totalmem = 13;
-    this.members.push({id:1,nick:'Bam',fname:'Kontee',position:'Team Leader'});
-    this.members.push({id:2,nick:'Waan',fname:'Porawee',position:'Planning Manager'});
-    this.members.push({id:3,nick:'Ti',fname:'Aphisit',position:'Planning'});
-    this.members.push({id:4,nick:'Muay',fname:'Arphaporn',position:'Planning'});
-    this.members.push({id:5,nick:'Nat',fname:'Jesada',position:'Deverlopment Manager'});
-    this.members.push({id:6,nick:'Bew',fname:'Narongrit',position:'Deverlopment'});
-    this.members.push({id:7,nick:'Fluke',fname:'Prachaya',position:'Deverlopment'});
-    this.members.push({id:8,nick:'Mos',fname:'Nattawut',position:'Quality assurerance Manager'});
-    this.members.push({id:9,nick:'Nut',fname:'Nuttawan',position:'Quality assurerance'});
-    this.members.push({id:10,nick:'Teen',fname:'Kasama',position:'Support Manager'});
-    this.members.push({id:11,nick:'Pipe',fname:'Siwakorn',position:'Support'});
-    this.members.push({id:12,nick:'Gell',fname:'Khemjira',position:'Support'});
+    this.members.push({id:1,nick:'Bam',fname:'Kontee',position:'Team Leader',img:'../img/bam.jpg'});
+    this.members.push({id:2,nick:'Waan',fname:'Porawee',position:'Planning Manager',img:'../img/waan.jpg'});
+    this.members.push({id:3,nick:'Ti',fname:'Aphisit',position:'Planning',img:'../img/ti.jpg'});
+    this.members.push({id:4,nick:'Muay',fname:'Arphaporn',position:'Planning',img:'../img/mauy.jpg'});
+    this.members.push({id:5,nick:'Nat',fname:'Jesada',position:'Deverlopment Manager',img:'../img/nat.jpg'});
+    this.members.push({id:6,nick:'Bew',fname:'Narongrit',position:'Deverlopment',img:'../img/bew.jpg'});
+    this.members.push({id:7,nick:'Fluke',fname:'Prachaya',position:'Deverlopment',img:'../img/fluke.jpg'});
+    this.members.push({id:8,nick:'Mos',fname:'Nattawut',position:'Quality assurerance Manager',img:'../img/mos.jpg'});
+    this.members.push({id:9,nick:'Nut',fname:'Nuttawan',position:'Quality assurerance',img:'../img/nut.jpg'});
+    this.members.push({id:10,nick:'Teen',fname:'Kasama',position:'Support Manager',img:'../img/teen.jpg'});
+    this.members.push({id:11,nick:'Pipe',fname:'Siwakorn',position:'Support',img:'../img/pipe.jpg'});
+    this.members.push({id:12,nick:'Gell',fname:'Khemjira',position:'Support',img:'../img/gell.jpg'});
     }
 
 }
